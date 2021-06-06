a1759b2e352c436bd6905abbea5d0c1a20b32c5b
CthulhuOnIce <cthulhuonice000@gmail.com>
2021-06-06 12:51:40 -0500

    forgot to remove these

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ sys.path.insert(1, "cogs")
 import prison
 import log
 import staffboard
-import scoreboard
 
 # from disputils import BotEmbedPaginator, BotConfirmation, BotMultipleChoice
 
@@ -37,7 +36,6 @@ bot = commands.Bot(
 prison.setup(bot, C)
 log.setup(bot, C)
 staffboard.setup(bot, C)
-scoreboard.setup(bot, C)
 
 @bot.event
 async def on_ready():  # I just like seeing basic info like this
