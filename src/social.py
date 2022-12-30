734665985c70872a7b09ffcb475486b0fdcdd777
CthulhuOnIce <cthulhuonice000@gmail.com>
2022-12-29 20:19:39 -0600

    hotfix

--- src/social.py
+++ src/social.py
@@ -32,19 +32,5 @@ class Social(commands.Cog):
             
             embed.add_field(name="Total Messages", value=db_user.total_messages)
 
-
-    @slash_command(name='simonsays', description='Repeat what Simon says.')
-    @option('text', str, description='The text to repeat')
-    async def player_info(self, ctx, text:str):
-        await ctx.respond("Simon says " + text, ephemeral=True)
-
-    @commands.user_command(name="Print Username")  # create a user command for the supplied guilds
-    async def player_information_click(self, ctx, member: discord.Member):  # user commands return the member
-        await ctx.respond(f"Hello {member.display_name}!")  # respond with the member's display name
-
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        return
-
 def setup(bot):
     bot.add_cog(Social(bot))
