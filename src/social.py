535651e9c1d2cbca844c0484ac0af21be1217380
CthulhuOnIce <cthulhuonice000@gmail.com>
2022-12-29 20:40:38 -0600

    vetting answers and notes

--- src/social.py
+++ src/social.py
@@ -32,6 +32,52 @@ class Social(commands.Cog):
                 embed.add_field(name="Total Reactions", value="\n".join([f"{reaction}: {db_user['reactions'][reaction]}" for reaction in db_user["reactions"]]), inline=False)
         
         await ctx.respond(embed=embed, ephemeral=ephemeral)
+    
+    @slash_command(name='vettinganswers', description='Get a user\'s vetting answers.')
+    @option('user', discord.User, description='The user to get answers for')
+    @option('ephemeral', bool, description='Whether to send the message as an ephemeral message')
+    async def vettinganswers(self, ctx, user:discord.User, ephemeral:bool=True):
+        if not ctx.author.guild_permissions.manage_roles:
+            await ctx.respond("You do not have permission to use this command.", ephemeral=True)
+            return
+        db_user = await db.get_user(user.id)
+        if not db_user:
+            await ctx.respond("User not found.", ephemeral=True)
+            return
+        if "verification" not in db_user:
+            await ctx.respond("User has not been vetted.", ephemeral=True)
+            return
+        embed = discord.Embed(title="Vetting Answers", description=f"Answers for {user}", color=0x00ff00)
+        for _ in db_user["verification"]:
+            question = _[0]
+            answer = _[1]
+            embed.add_field(name=question, value=answer, inline=False)
+        await ctx.respond(embed=embed, ephemeral=ephemeral)
+
+    @slash_command(name='notes', description='Get a user\'s admin notes.')
+    @option('user', discord.User, description='The user to get notes for')
+    @option('ephemeral', bool, description='Whether to send the message as an ephemeral message')
+    async def notes(self, ctx, user:discord.User, ephemeral:bool=True):
+        if not ctx.author.guild_permissions.manage_roles:
+            await ctx.respond("You do not have permission to use this command.", ephemeral=True)
+            return
+
+        notes = await db.get_notes(user.id)
+
+        if not notes:
+            await ctx.respond("User has no notes.", ephemeral=True)
+            return
+        
+        embed = discord.Embed(title="Notes", description=f"Notes for {user}", color=0x00ff00)
+        for note in notes:
+            author = self.bot.get_user(note["author"])
+            if not author:
+                author = await self.bot.fetch_user(note["author"])
+            if not author:
+                author = "Unknown"
+            embed.add_field(name=f'From {author} on {note["timestamp"]}', value=note["note"], inline=False)
+        await ctx.respond(embed=embed, ephemeral=ephemeral)
+
 
 def setup(bot):
     bot.add_cog(Social(bot))
