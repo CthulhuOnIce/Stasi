29b20f5c2295b9ce5331cf7dfd162cf723fa5bf7
CthulhuOnIce <41597119+CthulhuOnIce@users.noreply.github.com>
2023-02-15 03:26:07 -0600

    last_value

--- stasi.py
+++ stasi.py
@@ -91,7 +91,7 @@ async def on_application_command_error(ctx, error):  # share certain errors with
 
 @bot.event
 async def on_error(event, one, two):
-    error_raw = ''.join(traceback.format_exception(event))
+    error_raw = ''.join(traceback.format_exception(sys.last_value))
     errortracking.report_error(error_raw)
 
 bot.run(config.C["token"])
