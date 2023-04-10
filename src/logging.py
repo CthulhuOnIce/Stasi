a2028108270c5c9d5958d9c7258467c7d97559d2
CthulhuOnIce <cthulhuonice000@gmail.com>
2023-04-09 23:31:42 -0500

    emoji check on messages

--- src/logging.py
+++ src/logging.py
@@ -7,6 +7,8 @@ def log(category_broad, category_fine, message, print_message=True, preserve_new
     timestamp = datetime.datetime.now().strftime("%b %d %Y %H:%M:%S")
     if not preserve_newlines:
         message = message.replace("\n", " ")
+    # clean emojis, special characters
+    message = message.encode("ascii", "ignore").decode()
     if print_message: print(f"[{timestamp}] [{category_broad.upper()}] [{category_fine.upper()}] {message}")
     with open(f"logs/{category_broad.lower()}.log", "a+") as f:
         f.write(f"[{timestamp}] [{category_fine.upper()}] {message}\n")
