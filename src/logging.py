67f25e8e59bf6ba953c100fdb463a668775ac1d2
CthulhuOnIce <cthulhuonice000@gmail.com>
2023-04-10 03:28:49 -0500

    hm

--- src/logging.py
+++ src/logging.py
@@ -8,7 +8,7 @@ def log(category_broad, category_fine, message, print_message=True, preserve_new
     if not preserve_newlines:
         message = message.replace("\n", " ")
     # clean emojis, special characters
-    print_msg = message.encode("latin-1", "replace").decode()
+    print_msg = message.encode("latin-1", "replace").decode('latin-1')
     if print_message: print(f"[{timestamp}] [{category_broad.upper()}] [{category_fine.upper()}] {print_msg}")
     with open(f"logs/{category_broad.lower()}.log", "a+") as f:
         f.write(f"[{timestamp}] [{category_fine.upper()}] {print_msg}\n")
