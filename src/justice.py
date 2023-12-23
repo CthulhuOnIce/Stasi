4829da84e9d49f1a3a121af02cfafc714f4405d1
CthulhuOnIce <41597119+CthulhuOnIce@users.noreply.github.com>
2023-12-23 14:38:54 -0600

    oops

--- src/justice.py
+++ src/justice.py
@@ -440,8 +440,8 @@ class Justice(commands.Cog):
         case.registerUser(member)
         case.prosecutor_id = member.id
         
-        await case.newEvent("prosecutor_change", f"A New Prosecutor Has Been Appointed",
-                            f"Old Prosecutor: {case.nameUserByID(old_prosecutor)}\nNew Prosecutor: {case.nameUserByID(member.id)}")
+        case.event_log.append(await case.newEvent("prosecutor_change", f"A New Prosecutor Has Been Appointed",
+                            f"Old Prosecutor: {case.nameUserByID(old_prosecutor)}\nNew Prosecutor: {case.nameUserByID(member.id)}"))
         
         await case.Save()
         
